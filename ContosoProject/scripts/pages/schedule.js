d224229d0a5a4b3ced97ec97b9fcc2dc3189f770
MathibelaD <dineomathibela161@gmail.com>
2023-03-29 01:07:00 +0200

    input checkbox on shedule page

--- ContosoProject/scripts/pages/schedule.js
+++ ContosoProject/scripts/pages/schedule.js
@@ -19,6 +19,66 @@ const schedule = [
         "title": "New Technologies in Enterprise",
         "tracks": [2]
     },
+    {
+        "id": "session-5",
+        "title": "WebSockets and You",
+        "tracks": [1]
+    },
+    {
+        "id": "session-6",
+        "title": "Coffee and Cake Break",
+        "tracks": [1, 2]
+    },
+    {
+        "id": "session-7",
+        "title": "Building Responsive UIs",
+        "tracks": [1]
+    },
+    {
+        "id": "session-8",
+        "title": "Fun with Forms (no, really!)",
+        "tracks": [2]
+    },
+    {
+        "id": "session-9",
+        "title": "A Fresh Look at Layouts",
+        "tracks": [1]
+    },
+    {
+        "id": "session-10",
+        "title": "Real-world Applications of HTML5 APIs",
+        "tracks": [2]
+    },
+    {
+        "id": "session-11",
+        "title": "Lunch",
+        "tracks": [1, 2]
+    },
+    {
+        "id": "session-12",
+        "title": "Getting to Grips with JavaScript",
+        "tracks": [1]
+    },
+    {
+        "id": "session-13",
+        "title": "Transforms and Animations",
+        "tracks": [2]
+    },
+    {
+        "id": "session-14",
+        "title": "Web Design Adventures with CSS3",
+        "tracks": [1]
+    },
+    {
+        "id": "session-15",
+        "title": "Introducing Data Access and Caching",
+        "tracks": [2]
+    },
+    {
+        "id": "session-16",
+        "title": "Closing Thanks and Prizes",
+        "tracks": [1, 2]
+    }
 ];
 
 
